1454ed1996e2b9803becf1e79a3171d9be971505
Brittany Ladion-Lucero <bladionlucero@gmail.com>
2016-10-11 17:06:45 -0700

    assignment-25 work

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,22 @@
      ]
  };
 
+  // My Album for Assignment
+ var albumFF = {
+     title: 'The Fantasy of Finals',
+     artist: 'Enix Square',
+     label: 'EM',
+     year: '2006',
+     albumArtUrl: 'assets/images/album_covers/14.png',
+     songs: [
+         { title: 'Battle Theme', duration: '5:26' },
+         { title: 'Main Character Theme', duration: '3:40' },
+         { title: 'Opening Theme', duration: '2:53'},
+         { title: 'A Super Memorable Theme', duration: '3:12' },
+         { title: 'Chocobo Theme', duration: '4:59'}
+     ]
+ };
+
   var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -42,14 +58,15 @@
      return template;
  };
 
+      // #1
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
   var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
+
      // #2
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
@@ -67,4 +84,16 @@
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     var albums = [albumPicasso, albumMarconi, albumFF];
+     var index = 1;
+
+     albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if (index == albums.length){
+            var index = 0;
+        }
+     });
+
  };
